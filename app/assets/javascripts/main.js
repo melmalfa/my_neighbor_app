46df6c5455350a3e0f3d9fa806934a32457f037b
Melodie Malfa <melmalfa@gmail.com>
2015-03-23 13:37:12 -0400

    changes css and search formula

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,2 +1,2 @@
-L.mapbox.accessToken = 'pk.eyJ1IjoibWVsbWFsZmEiLCJhIjoidE9idmV6NCJ9.UUqoqiI7SZKG9u9d4dr_-Q';
-L.mapbox.map('map', 'melmalfa.lfnpijb5');
+// L.mapbox.accessToken = 'pk.eyJ1IjoibWVsbWFsZmEiLCJhIjoidE9idmV6NCJ9.UUqoqiI7SZKG9u9d4dr_-Q';
+// L.mapbox.map('map', 'melmalfa.lfnpijb5');
